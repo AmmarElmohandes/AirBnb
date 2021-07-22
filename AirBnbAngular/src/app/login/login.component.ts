2a74830a724bd44ce2fb3ce9d82d5f5759a08b4c
Muhammad Mamdouh Al-Naqeeb <alnaqeeb128@gmail.com>
2021-07-22 22:41:23 +0200

    Adding Login and fixing register json object

---
+++ AirBnbAngular/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private router:Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {}
+    invalidLogin = false;
+  form = new FormGroup({
+    userName: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required),
+  });
+  logIn(){
+    let credentials = this.form.value;
+    this.authService.logIn(credentials)
+    .subscribe(result =>{
+      if(result)
+     this.router.navigate(['']);
+
+      this.invalidLogin = true;
+    });
+
+  }
+}
